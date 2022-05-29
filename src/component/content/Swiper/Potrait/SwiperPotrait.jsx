59272c5a5b7fd71c98cf7923c0c06febb133bb6b
ivanerror <ivangabriel68@gmail.com>
2022-05-29 19:55:36 +0700

    commit fix standarization

--- src/component/content/Swiper/Potrait/SwiperPotrait.jsx
+++ src/component/content/Swiper/Potrait/SwiperPotrait.jsx
@@ -1,48 +1,53 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import PropTypes from 'prop-types'
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
+import 'swiper/css'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/pagination'
 
-import "./SwiperPotrait.css";
+import './SwiperPotrait.css'
 
 // import "./SwiperPotrait.css";
-import { Autoplay, EffectCoverflow, Pagination } from "swiper";
+import { Autoplay, EffectCoverflow, Pagination } from 'swiper'
 
-export default function SwiperPotrait(props) {
-  const { pathList } = props;
+export default function SwiperPotrait (props) {
+  const { pathList } = props
 
   return (
     <>
       <Swiper
-        effect={"coverflow"}
+        effect={'coverflow'}
         centeredSlides={true}
         autoplay={{
           delay: 3800,
-          disableOnInteraction: false,
+          disableOnInteraction: false
         }}
-        slidesPerView={"auto"}
+        slidesPerView={'auto'}
         coverflowEffect={{
           rotate: 50,
           stretch: 0,
           depth: 100,
           modifier: 1,
-          slideShadows: true,
+          slideShadows: true
         }}
         pagination={true}
         modules={[EffectCoverflow, Pagination, Autoplay]}
         className="swiper-potrait"
       >
-        {pathList.map((x) => {
+        {pathList.map((x, index) => {
           return (
-            <SwiperSlide>
+            <SwiperSlide key={index}>
               <img className="rounded-xl" src={`/img/slideshow/${x}`} />;
             </SwiperSlide>
-          );
+          )
         })}
       </Swiper>
     </>
-  );
+  )
+}
+
+SwiperPotrait.propTypes = {
+  pathList: PropTypes.array.isRequired
 }
